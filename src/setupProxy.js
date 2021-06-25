40f07041814bfebb3aca5f80f44ab6f90cf9e40a
unity <unityoxb@gmail.com>
2021-06-25 12:19:58 +0800

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/api/sliderApi',
+    app.use(createProxyMiddleware('/api',
         {
             target: "https://api.scifanchain.com",
             changeOrigin: true,
