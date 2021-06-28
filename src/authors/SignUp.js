893b5def6d7e226ff0a55bd0a3c2847962291959
unity <unityoxb@gmail.com>
2021-06-28 09:13:20 +0800

    up wws

--- src/authors/SignUp.js
+++ src/authors/SignUp.js
@@ -9,7 +9,7 @@ import { useHistory } from "react-router-dom";
 export default function SignUp () {
 
   const history = useHistory();
-  
+
   const [state, setState] = useState({
     username: "",
     email: "",
@@ -118,18 +118,18 @@ export default function SignUp () {
       username: state.username,
       email: state.email,
       password: state.password
-  }
+    }
 
     const storage = window.localStorage;
 
     axios({
       // Oauth2要求必须以表单形式提交
-      // headers: {
-      //     'Content-Type': 'application/x-www-form-urlencoded'
-      // },
       headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded'
       },
+      // headers: {
+      //     'Content-Type': 'application/json',
+      // },
       method: 'POST',
       url: 'https://api.scifanchain.com/authors/create_author/',
       // data: qs.stringify(authorInfo)
@@ -139,7 +139,7 @@ export default function SignUp () {
       const access_token = response.data.access_token;
       axios.defaults.headers.common["Authorization"] = access_token;
 
-      storage.scifanchain_username = state.username
+      storage.scifanchain_username = username
       storage.scifanchain_access_token = access_token
 
       console.log(response.data.access_token)
@@ -147,7 +147,6 @@ export default function SignUp () {
       console.log(response.data.exp)
 
       history.push('/space');
-
         // console.log(response.data.refresh_token)
     }).catch(err => {
         console.log(err)
