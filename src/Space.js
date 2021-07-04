c6e15affd3dda4f553616ce3c661d9190904a29d
unity <unityoxb@live.com>
2021-07-04 10:40:51 +0800

    update

--- src/Space.js
+++ src/Space.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, createRef, createContext, useMemo  } from 'react';
 import { Link } from 'react-router-dom'
-import { Grid, List, Header, Input } from 'semantic-ui-react';
+import { Grid, List, Header, Input, Button } from 'semantic-ui-react';
 import axios from 'axios';
 import StageForm from './story/StageForm'
 import Info from './authors/Info'
@@ -12,84 +12,118 @@ export const AuthorContext = createContext();
 
 export default function Space() {
 
-    const [loading, setLoading] = useState(true);
-    const [author, SetAuthor] =  useState({})
-    const [stages, SetStages] =  useState([])
-    const [error, setError] = useState('')  
+  let token = window.localStorage.getItem("scifanchain_access_token")
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 
-    const [content, setContent] = useState('')  
+  const [loading, setLoading] = useState(true);
+  const [author, SetAuthor] =  useState({})
+  const [stages, SetStages] =  useState([])
+  const [error, setError] = useState('')  
 
-    const onChange = (content) => {
-      setContent(content);
-    };
+  const [stageTitle, setStageTitle] = useState('')
+  const [stageContent, setStageContent] = useState('')  
 
-    const autofocusNoSpellcheckerOptions = useMemo(() => {
-      return {
-        autofocus: true,
-        spellChecker: false,
-      }
-    }, []);
+  const onBlurTitle = (e) => {
+    setStageTitle(e.target.value)
+    console.log(stageTitle)
+  }
 
-    useEffect(() => {
-        let token = window.localStorage.getItem("scifanchain_access_token")
-        axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+  const onChangeContent = (stageContent) => {
+    setStageContent(stageContent);
+  };
 
-        axios({
-            method: 'get',
-            url: 'https://api.scifanchain.com/authors/me/',
-        }).then(response => {
-            setLoading(false)
-            console.log(response.data)
-            SetAuthor(response.data)
-            SetStages(response.data.stages)
-            // console.log(response.data.refresh_token)
-        }).catch(err => {
-            setLoading(false)
-            setError('很抱歉，没有获取到数据！')
-            console.log(err)
-        });
-    }, [])
+  const autofocusNoSpellcheckerOptions = useMemo(() => {
+    return {
+      autofocus: true,
+      spellChecker: false,
+    }
+  }, []);
 
-    const stageList = stages.map((stage) => (
-        <List.Item key={stage.id} as={Link} to={
-            {
-                pathname: '/stage/' + stage.id,
-                stage_id: stage.id
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const submitData = {
+        title: stageTitle,
+        content: stageContent,
+    }
+    axios({
+        // Oauth2要求必须以表单形式提交
+        // headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        // },
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'post',
+        url: 'https://api.scifanchain.com/stages/create_stage/',
+        data: submitData
+    }).then(response => {
+        console.log(response)
+        // console.log(response.data.refresh_token)
+    }).catch(err => {
+        console.log(err)
+    });
+}
+
+  useEffect(() => {
+
+    axios({
+        method: 'get',
+        url: 'https://api.scifanchain.com/authors/me/',
+    }).then(response => {
+        setLoading(false)
+        console.log(response.data)
+        SetAuthor(response.data)
+        SetStages(response.data.stages)
+        // console.log(response.data.refresh_token)
+    }).catch(err => {
+        setLoading(false)
+        setError('很抱歉，没有获取到数据！')
+        console.log(err)
+    });
+  }, [])
+
+  const stageList = stages.map((stage) => (
+    <List.Item key={stage.id} as={Link} to={
+        {
+            pathname: '/stage/' + stage.id,
+            stage_id: stage.id
+        }
+    }>
+        {stage.title}
+    </List.Item>
+  ));
+
+const contextRef = createRef();
+
+  return (
+    <div ref={contextRef}>
+      <Grid>
+      
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <AuthorContext.Provider value={author}>
+              <Info />
+            </AuthorContext.Provider>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Input fluid placeholder='一个从脑海中浮同的想法...' style={{marginBottom: '1rem'}} onBlur={onBlurTitle}/>
+            <SimpleMDE 
+              value={stageContent} 
+              onChange={onChangeContent} 
+              options={autofocusNoSpellcheckerOptions}
+            />
+            <Button onClick={handleSubmit}>提交</Button>              
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Header>我的作品</Header>
+            {!loading && !error &&
+              <List>{stageList}</List>
             }
-        }>
-            {stage.title}
-        </List.Item>
-    ));
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-    const contextRef = createRef();
-
-    return (
-      <div ref={contextRef}>
-        <Grid>
-        
-          <Grid.Row>
-            <Grid.Column width={4}>
-              <AuthorContext.Provider value={author}>
-                <Info />
-              </AuthorContext.Provider>
-            </Grid.Column>
-            <Grid.Column width={8}>
-              <Input></Input>
-              <SimpleMDE 
-                value={content} 
-                onChange={onChange} 
-                options={autofocusNoSpellcheckerOptions}
-              />                        
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <Header>我的作品</Header>
-              {!loading && !error &&
-                <List>{stageList}</List>
-              }
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-
-      </div>
-    )
+    </div>
+  )
 }
