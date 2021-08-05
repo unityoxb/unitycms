5992adf53ea8e05413cddd25c4ee51e32a7286d7
unity <unityoxb@gmail.com>
2021-08-06 02:32:18 +0800

    add blog's model

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for scifan project.
+ASGI config for oxb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scifan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
